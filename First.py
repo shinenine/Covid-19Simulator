11c9464c86130d208a2e5eb79583ca2492986a77
yifan <1042750083@qq.com>
2021-03-24 22:32:46 +0800

    Update First.py

--- First.py
+++ First.py
@@ -8,12 +8,8 @@ import matplotlib.animation as animation
 from matplotlib.font_manager import FontProperties
 
 
-def DistanceCount(x1, y1, x2, y2):
-    return math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
-
-
 class People:
-    def __init__(self, x, y, age, token):
+    def __init__(self, x, y, token):
         """
         x, y: 表示这个人的坐标
         age: 表示年龄
@@ -29,16 +25,11 @@ class People:
         """
         self.x = x
         self.y = y
-        self.age = age
-        self.hiddenDay = 0
-        self.immune = False
         self.state = "susceptible"
-        self.infectedRate = float(0.005 * self.age)
-        self.deathRate = 0.0
-        self.inRoom = 0
-        self.roundPeople = 0
-        self.isCount = True
+        self.immune = False
         self.InHospital = False
+        self.rateEtoI = 0.125
+        self.isCount = True
         self.token = token
         # Susceptible, Infected, Removed, dead
 
@@ -55,89 +46,85 @@ class World:
         medicine: 康复率
         date:疾病开始爆发后经历的总天数
         quarantineRation:有自我隔离意识的人群比率
+        latency:潜伏期
+        NumofPeopleInfectedMeet患者每天见的人数
         """
         # self.people_container = [] # 用于存放people
-        self.bedQuantity = bedQuantity
-        self.deadRate = deadRate
-        self.reinfect = reinfected
-        self.SUSCEPTIBLE, self.INFECTED, self.REMOVED, self.DEAD = [], [], [], []  # 三种人群的数组
-        self.date = 0
-        self.medicine = 0.02
-        self.quarantineRatio = quarantineRatio
+
         self.numberOfPeople = numberOfPeople
+        self.bedQuantity = bedQuantity
+        self.peopleInHos = 0
+        self.deadRate = 0.065373
+        self.recoveryRate = 0.23
+        self.SUSCEPTIBLE, self.EXPOSED, self.INFECTED, self.REMOVED, self.DEAD = [], [], [], [], [] # 三种人群的数组
+        self.date = 0
+        self.NumofPeopleInfectedMeet = 10
+        self.infectedRate = 0.6
+        self.reinfect = reinfected
+        self.peopleInfectedRate = 0
 
     def initialize_container(self):
         """
-        产生6000个人, 坐标, 年龄都是随机
+        产生10000000个人, 坐标是随机
         """
-        for i in range(int(self.numberOfPeople * self.quarantineRatio)):
+        for i in range(int(self.numberOfPeople)):
             x = Decimal(random.uniform(0, 100)).quantize(Decimal("0.0"))
             y = Decimal(random.uniform(0, 100)).quantize(Decimal("0.0"))
-            age = random.randint(1, 80)
-            new_people = People(x, y, age, True)
+            new_people = People(x, y, True)
             self.SUSCEPTIBLE.append(new_people)
-        for i in range(int(self.numberOfPeople * (1 - self.quarantineRatio))):
+
+        for i in range(2):  # 产生40个零号病人
             x = Decimal(random.uniform(0, 100)).quantize(Decimal("0.0"))
             y = Decimal(random.uniform(0, 100)).quantize(Decimal("0.0"))
-            age = random.randint(1, 80)
-            new_people = People(x, y, age, False)
-            self.SUSCEPTIBLE.append(new_people)
-        for i in range(2):  # 产生两个零号病人
-            x = Decimal(random.uniform(0, 100)).quantize(Decimal("0.0"))
-            y = Decimal(random.uniform(0, 100)).quantize(Decimal("0.0"))
-            age = 40
-            new_people = People(x, y, age, True)
-            new_people.hiddenDay = 4
-            new_people.deathRate = 0
+            new_people = People(x, y, False)
             self.INFECTED.append(new_people)
 
     def UpdateSusInfo(self):
         """
-        更新易感人群的基本信息，判断该人是否会成为感染者，并为其随机生成一个固定范围内的潜伏日期.初始化的住院日期由当天的医疗水平决定
+        更新易感人群的基本信息，判断该人是否会成为潜伏者
         """
         count = 0
+        
         for people in self.SUSCEPTIBLE:  # 遍历所有的健康易感人群
             """
             修改易感人群的感染率
             若某易感者没有自主隔离意识，则会移动到其他地方
             """
             if people.token:
-                people.infectedRate = 0.06 * (1 - self.medicine)
+                pass
+                #people.infectedRate = 0.06 * (1 - self.medicine)
             else:
                 deltaX = random.randint(-1, 1)
                 deltaY = random.randint(-1, 1)
                 people.x += deltaX
                 people.y += deltaY
-                if 0.01 * people.age >= 0.1:
-                    people.infectedRate = (0.01 * people.age) * pow(1.07, people.roundPeople) * (1 - self.medicine)
-                else:
-                    people.infectedRate = 0.1 * pow(1.07, people.roundPeople) * (1 - self.medicine)
-            if people.infectedRate >= 0.8:
-                people.infectedRate = 0.8
-            elif people.infectedRate < 0:
-                people.infectedRate = 0
-            if people.roundPeople <= 0:
-                people.infectedRate = 0
-            # 当易感者周围的患病者大于1时，易感者就有了自我隔离意识，不再移动
-            if people.roundPeople > 1:
-                people.token = True
-            index = np.random.choice([1, 0], 1, p=[people.infectedRate, 1 - people.infectedRate])  # index 为0，表示不会感染
-
+            
+            index = np.random.choice([1, 0], 1, p=[self.peopleInfectedRate, 1 - self.peopleInfectedRate])  # index 为0，表示不会感染
             if index[0] == 1:
-                people.state = 'infected'
-                print("正常人被感染")
-                if people.roundPeople >= 1:
-                    print("\n感染者的感染率：", people.infectedRate, "周围人数：", people.roundPeople, "\n")
+                people.state = 'exposed'
                 count += 1
-                people.hiddenDay = random.randrange(4, 7)  # 生成一个随机的潜伏日期
-                if people.age > 40:  # 致死率初始化
-                    people.deathRate = float(self.deadRate * people.age / 10)
-                people.inRoom = int(0.2 * people.age)
+                #print("正常人被感染")
+                
                 people.isCount = True
-                # 将这个新患者加入到感染者队列
-                self.INFECTED.append(people)
+                # 将这个新患者加入到潜伏者队列
+                self.EXPOSED.append(people)
                 self.SUSCEPTIBLE.remove(people)
         print("疫情爆发第", self.date + 1, "天， 日感染人数：", count)
+
+    def UpdateExpInpo(self):
+        count = 0
+        for people in self.EXPOSED:
+            if people.isCount:
+                pass
+
+            toIChoice = np.random.choice([1, 0], 1, p=[people.rateEtoI, 1 - people.rateEtoI])
+            if toIChoice[0]:
+                people.isCount = True
+                people.state = "infected"
+                self.INFECTED.append(people)
+                self.EXPOSED.remove(people)
+                count += 1
+        print("疫情爆发第", self.date + 1, "天， 潜伏者爆发人数：", count)
 
     def updateInfectorInfo(self):
         """
@@ -150,80 +137,47 @@ class World:
             temp_size -= 1
             if temp_size < 0:
                 break
-            # 每过一天，感染者的潜伏日期或者住院日期减少一天
+
+            if people.isCount:
+                pass
+
+            # 住院的人每天都有概率康复
             if people.InHospital:
-                people.inRoom -= 1
-                if people.inRoom <= 0:  # 痊愈
+                recoChoice = np.random.choice([1, 0], 1, p=[self.recoveryRate, 1 - self.recoveryRate])
+                if recoChoice[0]:
                     people.isCount = True
-                    people.deathRate = 0.1
-                    people.infectedRate = 0.4
+                    people.state = "recovered"
                     if self.reinfect:
                         self.REMOVED.append(people)
                     else:
                         self.SUSCEPTIBLE.append(people)
                     self.INFECTED.remove(people)
                     self.bedQuantity += 1
-                    # print("患者痊愈")
+                    self.peopleInHos -= 1
                     continue
-                else:
-                    # print("患者仍在医院治疗,还需天数：", people.inRoom)
-                    pass
 
             else:
-                people.hiddenDay -= 1
-                if people.hiddenDay <= 0:
-                    if self.bedQuantity > 0:  # 住院成功
-                        self.bedQuantity -= 1
-                        people.InHospital = True
-                        # print("住院成功")
-                    else:
-                        people.hiddenDay = 1
-                        # print("未能成功住院")
-
-            index = np.random.choice([1, 0], 1, p=[people.deathRate, 1 - people.deathRate])  # index 为0，表示不会死亡
+                if self.bedQuantity > 0:  # 住院成功
+                    self.bedQuantity -= 1
+                    self.peopleInHos += 1
+                    people.InHospital = True
+                    # print("住院成功")
+            
+            index = np.random.choice([1, 0], 1, p=[self.deadRate, 1 - self.deadRate])  # index 为0，表示不会死亡
             # print(index, "\t", people.infectedRate)
 
             if index[0] == 1:
-                print("患者死亡...其死亡率此时为", people.deathRate, "年龄为", people.age, "是否死于医院：", people.InHospital)
+                #print("患者死亡...其死亡率此时为", self.deathRate, "年龄为", people.age, "是否死于医院：", people.InHospital)
                 people.state = 'dead'
                 people.isCount = True
                 # people.deathRate = 0
                 # people.infectedRate = 0
                 if people.InHospital:
                     self.bedQuantity += 1
+                    self.peopleInHos -= 1
                 self.DEAD.append(people)
                 self.INFECTED.remove(people)
                 continue
-
-            else:  # 患者存活，则修改病人的死亡率,同时进行疾病传播
-                if people.hiddenDay > 0 and people.age > 40:  # 针对40岁以上，仍处于潜伏期的人，死亡率增高
-                    people.deathRate += self.deadRate
-                elif people.hiddenDay <= 0 and people.age > 40:  # 针对40岁以上且已经住院的人
-                    people.deathRate = self.deadRate * people.age / 10
-
-                if self.date < 4 and people.InHospital is False:  # 4为默认的第一潜伏期时间
-                    temp_count = 2
-                    for survival in self.SUSCEPTIBLE:  # 指数传播,每次感染有效距离内最多两个人
-                        if DistanceCount(people.x, people.y, survival.x, survival.y) <= 10:
-                            survival.state = "infected"
-                            # print("指数增长下感染了一人")
-                            survival.hiddenDay = random.randrange(6, 12)
-                            if people.age > 40:  # 致死率更新
-                                people.deathRate = float(0.001 * people.age)
-                            people.inRoom = 20 * people.age * 0.01
-                            # 将这个新患者加入到感染者队列
-                            self.INFECTED.append(survival)
-                            self.SUSCEPTIBLE.remove(survival)
-
-                            temp_count -= 1
-                            if temp_count <= 0:
-                                break
-
-                if (self.date >= 4) and people.isCount and (people.InHospital is False):
-                    people.isCount = False
-                    for survival in self.SUSCEPTIBLE:
-                        if DistanceCount(people.x, people.y, survival.x, survival.y) <= 3.0:
-                            survival.roundPeople += 1
 
     def UpdateRemInfo(self):
         """
@@ -233,35 +187,38 @@ class World:
             if people.isCount:
                 people.isCount = False
                 people.immune = True
-                people.state = 'removed'
-                for survival in self.SUSCEPTIBLE:
-                    if DistanceCount(people.x, people.y, survival.x, survival.y) <= 3:
-                        if survival.roundPeople >= 1:
-                            survival.roundPeople -= 1
 
         for people in self.DEAD:
             if people.isCount:
                 people.isCount = False
-                for survival in self.SUSCEPTIBLE:
-                    if DistanceCount(people.x, people.y, survival.x, survival.y) <= 3:
-                        if survival.roundPeople >= 1:
-                            survival.roundPeople -= 1
 
     def Happen(self):
         self.initialize_container()
         self.maxInfect = 2
-        fig = plt.figure(figsize=(100, 100))
+        fig = plt.figure(figsize=(50, 50))
         while len(self.INFECTED) > 0 and len(self.SUSCEPTIBLE) > 0:
             self.Draw(False, fig)
             self.date += 1
+            self.peopleInfectedRate = 1 - pow(1 - self.NumofPeopleInfectedMeet / (self.numberOfPeople-len(self.DEAD)) * self.infectedRate, len(self.INFECTED) - self.peopleInHos)
+            if self.date == 7:
+                self.deadRate = 0.05373
+            if self.date == 14:
+                self.deadRate = 0.035373
+                self.NumofPeopleInfectedMeet = 8
+            if self.date == 28:
+                self.NumofPeopleInfectedMeet = 1.5
+                self.recoveryRate = 0.95
+            if self.date == 42:
+                self.NumofPeopleInfectedMeet = 1
+                self.deadRate = 0.0173
+
             print("当前是疫情爆发的第", self.date, "天")
             self.UpdateRemInfo()
             print("治愈人数当前共有", len(self.REMOVED))
             self.updateInfectorInfo()
             print("当前患者总计人数", len(self.INFECTED))
+            self.UpdateExpInpo()
             self.UpdateSusInfo()
-            if self.medicine < 1:
-                self.medicine += 0.01
             print("易感人群总计人数", len(self.SUSCEPTIBLE))
             print("医院床位还有", self.bedQuantity)
             print("当前死亡人数为", len(self.DEAD))
@@ -269,7 +226,7 @@ class World:
             if len(self.INFECTED) <= 0 or len(self.SUSCEPTIBLE) <= 0:
                 print("sleep")
                 time.sleep(10)
-                self.Draw(False)
+                self.Draw(False, fig)
             if len(self.INFECTED) > self.maxInfect:
                 self.maxInfect = len(self.INFECTED)
 
@@ -283,15 +240,14 @@ class World:
                   len(self.DEAD + self.REMOVED))
 
     def Draw(self, choose, fig):
-        myFont = FontProperties(fname='HYShangWeiShouShuW.ttf', size=15)
+        # myFont = FontProperties(fname='HYShangWeiShouShuW.ttf', size=15)
         plt.rcParams['axes.unicode_minus'] = False
         left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
 
         aGraphic = fig.add_axes([left, bottom, width, height])
-        day = "Days:" + str(self.date) + "  当前患者人数:" + str(len(self.INFECTED)) + "  当前死亡人数:" + str(
-            len(self.DEAD)) + "  尚未感染人数：" + str(len(self.SUSCEPTIBLE) + "当前治愈人数" + str(len(self.REMOVED)))
-        aGraphic.set_title(day, fontproperties=myFont)
-        npx1, npy1, npx2, npy2, npx3, npy3, npx4, npy4 = [], [], [], [], [], [], [], []
+        day = "Days:" + str(self.date) + "  S尚未感染人数：" + str(len(self.SUSCEPTIBLE)) + "E潜伏者人数:" + str(len(self.EXPOSED)) + "I当前患者人数:" + str(len(self.INFECTED)) +"R当前治愈人数" + str(len(self.REMOVED)) + "  D当前死亡人数:" + str(len(self.DEAD))
+        aGraphic.set_title(day)  # , fontproperties=myFont)
+        npx1, npy1, npx2, npy2, npx3, npy3, npx4, npy4, npx5, npy5 = [], [], [], [], [], [], [], [], [], []
         for people in self.SUSCEPTIBLE:
             npx1.append(people.x)
             npy1.append(people.y)
@@ -304,18 +260,22 @@ class World:
         for people in self.DEAD:
             npx4.append(people.x)
             npy4.append(people.y)
+        for people in self.EXPOSED:
+            npx5.append(people.x)
+            npy5.append(people.y)
         aGraphic.scatter(npx1, npy1, marker="o", color="blue", s=5, label="SUSCEPTIBLE")
+        aGraphic.scatter(npx5, npy5, marker="o", color="yellow", s=5, label="EXPOSED")
         aGraphic.scatter(npx2, npy2, marker="o", color="red", s=5, label="INFECTED")
         aGraphic.scatter(npx3, npy3, marker="o", color="green", s=5, label="REMOVED")
         aGraphic.scatter(npx4, npy4, marker="o", color="white", s=5, label="DEAD")
         # ani = animation.FuncAnimation(aGraphic, )
         aGraphic.legend(loc="best")
         if not choose:
-            plt.pause(1)
+            plt.pause(0.1)
             aGraphic.cla()
         # else:
         #     plt.show()
 
 
-start = World(100, 0.0001, True, 0.33, 6000)  # 床位，杀伤力，治愈者是否带有抗体
+start = World(600, 0.0001, True, 0.33, 100000)  # 床位，杀伤力，治愈者是否带有抗体
 start.Happen()
